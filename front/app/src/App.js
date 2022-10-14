c2bed5e565d522fe6c9f68bd29e133bbc4372020
unknown <remi.chabrant@gmail.com>
2022-10-14 17:28:23 +0200

    firebase init

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
+import db from "./firebase-config.js";
+import React, {useState, useEffect } from "react";
+import {collection, getDocs} from "firebase/firestore";
+
+
 function App() {
+
+    const [services, setServices] = useState([]);
+
+    const servicesCollectionRef = collection(db, "services");
+
+    useEffect(() => {
+        const getServices = async() => {
+            const data = await getDocs(servicesCollectionRef);
+            setServices(data.docs.map((doc) => ({ ...doc.data(), id:doc.id })));
+        }
+
+        getServices();
+    }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+              <p>Office Queue Management</p>
+              <div>
+              {services.map((service) => (
+
+                  <h1 key={service.name}>{service.name}</h1>
+
+              ))}
+          </div>
       </header>
+          
     </div>
   );
 }
