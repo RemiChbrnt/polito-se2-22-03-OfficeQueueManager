e2cc04995c3f1ab73c0bef93fcaf4bc993a97763
LorenzoSciara <s303452@studenti.polito.it>
2022-10-19 22:34:07 +0200

    delete ticket button and css in DeskOfficer

--- front/app/src/screens/deskOfficer.js
+++ front/app/src/screens/deskOfficer.js
@@ -9,7 +9,7 @@ const DeskOfficerView = () => {
 
   const [currentCustomer, setCurrentCustomer] = useState("");
 
-  async function action (){
+  async function action() {
     const customer = await API.nextClient("Desk1");
     setCurrentCustomer(customer);
   }
@@ -20,13 +20,14 @@ const DeskOfficerView = () => {
       <Row>
         <Col md={10}>
           <h1>Desk Officer</h1>
-          <Card>
+          <ul></ul>
+          {/*<Card>
             <Card.Title>Currently serving client: *******</Card.Title>
             <Card.Text>
                 Currently serving : {currentCustomer}
             </Card.Text>
             <Button variant="primary" onClick={()=>action()}><span>Next Client</span></Button>
-          </Card>
+          </Card>*/}
         </Col>
         <Col md={2}>
           <div className="d-grid gap-2">
@@ -35,6 +36,25 @@ const DeskOfficerView = () => {
         </Col>
       </Row>
       <ul></ul>
+      <Row>
+        <h2>Currently serving</h2>
+        <ul></ul>
+        {currentCustomer.length === 0 ?
+          <p><b>No served Client. Press the button Next Client to serve a Client.</b></p>
+          : <p><b>Ticket ID:</b> {currentCustomer}</p>
+        }
+        {console.log(currentCustomer.length)}
+      </Row>
+
+      <Row>
+        <Col md={10}>
+        </Col>
+        <Col md={2}>
+          <div className="d-grid gap-2">
+            <Button variant="white" size="lg" style={{ backgroundColor: "#00706c" }} onClick={() => action()}><h4 className="text-white">Next Client</h4></Button>
+          </div>
+        </Col>
+      </Row>
     </Container>
 
   );
